6a6972d3d283d1e817dbba00a67e54650b9db130
Jerry Eshbaugh <Jerry.Eshbaugh@ocio.usda.gov>
2020-03-09 23:11:07 -0600

    Get iterations working for each row

--- Readme.md
+++ Readme.md
@@ -2,3 +2,10 @@
 ## Dot Net Utils
 
 Misc dot net utils for future reference
+
+## Getting Startd
+
+To run a power shell script you will need to either sign the script or loosen security...
+Run this from the command line where you are executing the script to relax security
+Set-ExecutionPolicy -Scope CurrentUser
+ExecutionPolicy: RemoteSigned
--- csv2xml.ps1
+++ csv2xml.ps1
@@ -1,3 +1,14 @@
+$P = Import-Csv ./Data/simple-sample.csv -Delimiter ',' 
+Write-Output "Imported CSV...", $P
 
-$P = Get-Process
-Write-Output $P
+$P | Group-Object Id -ov grp |
+
+Group-Object Id -ov grp | ForEach-Object {
+    Write-Output "-->",$_.Group
+    ForEach ($ii in $_.Group) {
+        Write-Output "***>",$ii
+    }
+   }
+#  $contacts = foreach ($Phone in $_.Group) {
+#   $ExecutionContext.InvokeCommand.ExpandString($entryTemplate)  
+
