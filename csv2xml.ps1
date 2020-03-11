ef0a92303a99b4d89aa184ac75a358db17b09c9b
Jerry Eshbaugh <Jerry.Eshbaugh@ocio.usda.gov>
2020-03-10 21:03:27 -0600

    Simple case ready for xml output

--- csv2xml.ps1
+++ csv2xml.ps1
@@ -1,14 +1,63 @@
+
+
+$entryTemplate = @'
+Firstname ="$($row.Group.Firstname)" Lastname= "$($row.Group.Lastname)"
+'@
+
+$docTemplate = @'
+<contact $($contacts -join "`n") />
+'@
+
 $P = Import-Csv ./Data/simple-sample.csv -Delimiter ',' 
 Write-Output "Imported CSV...", $P
 
 $P | Group-Object Id -ov grp |
 
 Group-Object Id -ov grp | ForEach-Object {
-    Write-Output "-->",$_.Group
-    ForEach ($ii in $_.Group) {
-        Write-Output "***>",$ii
+#    Write-Output "-->",$_.Group
+    $contacts = ForEach ($row in $_.Group) {
+#        Write-Output "***>",$ii.Group
+
+        $ExecutionContext.InvokeCommand.ExpandString($entryTemplate)  
+
     }
-   }
+}
+
+Write-Output "Out:", $contacts
+
+
+
 #  $contacts = foreach ($Phone in $_.Group) {
 #   $ExecutionContext.InvokeCommand.ExpandString($entryTemplate)  
 
+
+# Original Code with issue 
+<#
+
+$docTemplate = @'
+<contact $($contacts -join "`n") />
+'@
+$entryTemplate = @'
+Firstname ="$($Phone.Firstname)" Lastname= "$($Phone.Lastname) Email= "$($Phone.Email) Phone= "$($Phone.Phone)" Room= "$($Phone.Room)" Phone= "$($Phone.Phone)"
+'@
+
+Import-Csv Test.csv -Delimiter ',' | Group-Object Id -ov grp | ForEach-Object {
+  $contacts = foreach ($Phone in $_.Group) {
+    $ExecutionContext.InvokeCommand.ExpandString($entryTemplate)  
+  }
+ $ExecutionContext.InvokeCommand.ExpandString($docTemplate) } | 
+  Set-Content -LiteralPath file.xml
+
+#>
+
+
+#Desired Output ...
+#<?xml version="1.0" encoding="UTF-8"?>
+#<TSP version="1.1">
+#    <contact firstname="Bob" lastname="Smith" email="Bobs@email.com" Phone="1111" room="Suite 101" id="1"/>
+#    <contact firstname="John" lastname="Doe" email="John@email.com" phone="2222" room="Suite 102" id="2"/>
+#</TSP>
+
+#Output produced by original code...
+#<contact Firstname ="Bob" Lastname= "Smith Email= "Bobs@email.com Phone= "1111" Room= "Suite 101" Phone= "1111" />
+#<contact Firstname ="John" Lastname= "Doe Email= "John@email.com Phone= "2222" Room= "Suite 102" Phone= "2222" />
